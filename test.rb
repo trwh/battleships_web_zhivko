cb48836d6b6aca546786417e4aafa304db09f2e4
zsid <zhivko.siderov@gmail.com>
2015-09-16 11:42:30 +0100

    Creating HTML method

--- test.rb
+++ test.rb
@@ -7,7 +7,7 @@ cell_names = {'A'=>1, 'B'=>2, 'C'=>3, 'D'=>4, 'E'=>5, 'F'=>6}
   # </tr>
   # <tr>
   #   <td>test4</td>
-  # </tr>
+  # </tr> <tr>
 
 def print_stuff(cell_names)
 index = 0
@@ -16,15 +16,31 @@ index = 0
     index=index+3
   end
 end
+#
+# p print_stuff(cell_names)
 
-p print_stuff(cell_names)
+def print_table2(cell_names)
+  result = ""
+  index = 0
+  cell_names.size.times do |index|
+  end
+  cell_names.keys.each do |el|
+    result += "<td>#{el}</td>"
+  end
+  "<tr>\n #{result} \n </tr>"
+end
 
 def print_table(cell_names)
-result = ""
-cell_names.keys.each do |el|
-  result += "<td>#{el}</td>"
-end
-result
+  result = ""
+  cell_names.keys.each_slice(3) do |slice|
+    new_result = "<tr>\n"
+    slice.each do |element|
+      new_result << "<td>#{element}</td>\n"
+    end
+    new_result << "</tr>\n"
+    result << new_result
+  end
+  result
 end
 
 p print_table(cell_names)
