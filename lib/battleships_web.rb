61704f1eefd92f887217b8f1b7bcff0ebafaba00
zsid <zhivko.siderov@gmail.com>
2015-09-17 09:31:47 +0100

    Fixed placing the ship

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,8 +2,6 @@ require 'sinatra/base'
 require_relative 'board'
 
 $board = Board.new(Cell)
-ship = Ship.new(3)
-$board.place(ship, :C3, :vertically)
 
 class Battle_ship_september < Sinatra::Base
 
@@ -17,8 +15,8 @@ class Battle_ship_september < Sinatra::Base
 
   get '/new_game' do
     @player = params[:name]
-    # @ship = Ship.new(3)
-    # $board.place(@ship, :A3, :vertically)
+    @ship = Ship.new(3)
+    $board.place(@ship, :A3, :vertically)
     erb :new_player
   end
 
